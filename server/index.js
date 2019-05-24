63caf8babee0f7b3c2dbc0a33990b65e5b233cf1
Curt Bryan <kurdtbryan@gmail.com>
2019-05-24 10:38:11 -0700

    backenddone

---
+++ server/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const port = 4002;
+app.use(express.json());
+require("dotenv").config();
+const massive = require("massive");
+
+massive(process.env.CONNECTION_STRING).then(db => {
+  app.set("db", db);
+  console.log("connected to db");
+});
+
+app.get("/api/inventory", (req, res) => {
+  const db = req.app.get("db");
+  db.get_inventory().then(products => {
+    res.status(200).send(products);
+  });
+});
+
+app.post("/api/inventory", (req, res) => {
+  const { name, price, img } = req.body;
+  const db = req.app.get("db");
+  db.add_to_inventory([name, price, img])
+    .then(products => {
+      res.status(200).send(products);
+    })
+    .catch(err => console.log(err));
+});
+
+app.put("/api/inventory/:id", (req, res, next) => {
+  const db = req.app.get("db");
+  const { id } = req.params;
+  const { name, price, img } = req.query;
+  db.update_product([id, name, price, img])
+    .then(products => {
+      res.status(200).send(products);
+    })
+    .catch(err => console.log(err));
+});
+
+app.delete("/api/inventory/:id", (req, res, next) => {
+  const db = req.app.get("db");
+  const { id } = req.params;
+  db.delete_product([id])
+    .then(products => {
+      res.status(200).send(products);
+    })
+    .catch(err => console.log(err));
+});
+
+app.listen(port, () => console.log(`server is on ${port}`));
